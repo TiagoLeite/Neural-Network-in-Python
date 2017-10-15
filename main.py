1a7b16927900a5d238b9c647b8f813abddc56291
Tiago Leite <tiago.tmleite@gmail.com>
2017-10-15 18:28:57 -0200

    convolutional from example (deep mnist for experts)

--- main.py
+++ main.py
@@ -1,99 +1,81 @@
+from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf
 import os
-from tensorflow.examples.tutorials.mnist import input_data
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-mnist = input_data.read_data_sets("/tmp/data", one_hot=True)
 
-#  Defining our model:
-n_classes = 10
-batch_size = 128
-x = tf.placeholder('float', [None, 784])  # matrix to single array (28x28 mnist)
-y = tf.placeholder('float', [None, 10])
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+
+def bias_variable(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
 
 
 def conv2d(x, W):
     return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
 
-def maxpool2d(x):
+def max_pool_2x2(x):
     return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-#                               size of window    movement of window
 
 
-def convolutional_neural_network(x):
+print("Reading mnist...")
+# sess = tf.InteractiveSession()
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
-    weigths = {'w_conv1': tf.Variable(tf.random_normal([5, 5, 1, 32])),
-               'w_conv2': tf.Variable(tf.random_normal([5, 5, 32, 64])),
-               'w_fc': tf.Variable(tf.random_normal([7*7*64, 1024])),
-               'out': tf.Variable(tf.random_normal([1024, n_classes]))}
+x = tf.placeholder(tf.float32, shape=[None, 784])
+y_ = tf.placeholder(tf.float32, shape=[None, 10])
 
-    biases = {'b_conv1': tf.Variable(tf.random_normal([32])),
-              'b_conv2': tf.Variable(tf.random_normal([64])),
-              'b_fc': tf.Variable(tf.random_normal([1024])),
-              'out': tf.Variable(tf.random_normal([n_classes]))}
+# Convolutional Layer 1:
+W_conv1 = weight_variable([5, 5, 1, 32])
+b_conv1 = bias_variable([32])
+x_image = tf.reshape(x, [-1, 28, 28, 1])
+h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1)+b_conv1)
+h_pool1 = max_pool_2x2(h_conv1)
 
-    x = tf.reshape(x, shape=[-1, 28, 28, 1])
+# Convolutional Layer 2:
+W_conv2 = weight_variable([5, 5, 32, 64])
+b_conv2 = bias_variable([64])
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2)+b_conv2)
+h_pool2 = max_pool_2x2(h_conv2)
 
-    conv1 = conv2d(x, weigths['w_conv1'])
-    conv1 = maxpool2d(conv1)
+# Fully connected layer:
+W_fc1 = weight_variable([7*7*64, 1024])
+b_fc1 = bias_variable([1024])
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1)+b_fc1)
 
-    conv2 = conv2d(conv1, weigths['w_conv2'])
-    conv2 = maxpool2d(conv2)
-    fc = tf.reshape(conv2, [-1, 7*7*64])
-    fc = tf.nn.relu(tf.matmul(fc, weigths['w_fc']) + biases['b_fc'])
+# Dropout:
+keep_prob = tf.placeholder(tf.float32)
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
-    output = tf.matmul(fc, weigths['out'])+biases['out']
+# Read out layer:
+W_fc2 = weight_variable([1024, 10])
+b_fc2 = bias_variable([10])
 
-    return output
+y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
 
-def train_neural_network(x):  # x is input data
+train_step = tf.train.AdamOptimizer().minimize(cross_entropy)
+correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-    prediction = convolutional_neural_network(x)
-    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=prediction))
-    optimizer = tf.train.AdamOptimizer().minimize(cost)
-    # optimizer = tf.train.GradientDescentOptimizer(0.01).minimize(cost)
-    epochs = 10
+print("Training...")
 
-    with tf.Session() as sess:
+with tf.Session() as sess:
 
-        sess.run(tf.global_variables_initializer())
-        for epoch in range(epochs):
-            epoch_loss = 0
-            for _ in range(int(mnist.train.num_examples/batch_size)):
-                epoch_x, epoch_y = mnist.train.next_batch(batch_size)
-                _, c = sess.run([optimizer, cost], feed_dict={x: epoch_x, y: epoch_y})
-                epoch_loss += c
-            print('Epoch ', epoch, " completed out of ", epochs, " with loss: ", epoch_loss)
-        correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
-        accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
-        print("Accuracy: ", accuracy.eval({x: mnist.test.images, y: mnist.test.labels}))
+    sess.run(tf.global_variables_initializer())
 
+    for i in range(2000):
+        batch = mnist.train.next_batch(50)
+        if i % 100 == 0:
+            train_acc = accuracy.eval(feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
+            print('Step %d, training accuracy %g' % (i, train_acc))
+        train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
-train_neural_network(x)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    print('Test accuracy %g' % accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
