32180b62135afea134a2027b62f34425e16211db
Tiago Leite <tiago.tmleite@gmail.com>
2017-10-31 23:54:14 -0200

    big train and test cifar 10

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import os
 import sys
 import datetime
+import random
 import pickle
 import numpy as np
 
@@ -14,20 +15,14 @@ keep_prob = tf.placeholder(tf.float32)  # for dropout
 
 
 def mix_channels(array_rgb):
-
     size = int(len(array_rgb)/3)
     array = [float]*size
-
     # print("RGB:\n", array_rgb)
-
     for k in range(0, int(size)):
         array[k] = array_rgb[k] + array_rgb[k+1024]*256 + array_rgb[k+1024]*256*256
         array[k] /= (np.power(2, 24)-1)
-
     # print(array)
-
     # print("max = ", max(array))
-
     return array
 
 
@@ -48,13 +43,13 @@ def maxpool2d(x):
 
 def convolutional_neural_network(x):
 
-    weigths = {'w_conv1': tf.Variable(tf.random_normal([3, 3, 1, 32])),
-               'w_conv2': tf.Variable(tf.random_normal([4, 4, 32, 64])),
-               'w_fc': tf.Variable(tf.random_normal([8*8*64, 1024])),
+    weigths = {'w_conv1': tf.Variable(tf.random_normal([3, 3, 1, 64])),
+               'w_conv2': tf.Variable(tf.random_normal([4, 4, 64, 128])),
+               'w_fc': tf.Variable(tf.random_normal([8*8*128, 1024])),
                'out': tf.Variable(tf.random_normal([1024, n_classes]))}
 
-    biases = {'b_conv1': tf.Variable(tf.random_normal([32])),
-              'b_conv2': tf.Variable(tf.random_normal([64])),
+    biases = {'b_conv1': tf.Variable(tf.random_normal([64])),
+              'b_conv2': tf.Variable(tf.random_normal([128])),
               'b_fc': tf.Variable(tf.random_normal([1024])),
               'out': tf.Variable(tf.random_normal([n_classes]))}
 
@@ -66,7 +61,7 @@ def convolutional_neural_network(x):
     conv2 = tf.nn.relu(conv2d(conv1, weigths['w_conv2'])+biases['b_conv2'])
     conv2 = maxpool2d(conv2)
 
-    fc = tf.reshape(conv2, [-1, 8*8*64])
+    fc = tf.reshape(conv2, [-1, 8*8*128])
     fc = tf.nn.relu(tf.matmul(fc, weigths['w_fc']) + biases['b_fc'])
     fc = tf.nn.dropout(fc, keep_prob)
 
@@ -80,65 +75,83 @@ def train_neural_network(x):  # x is input data
     prediction = convolutional_neural_network(x)
     cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=prediction))
     optimizer = tf.train.AdamOptimizer().minimize(cost)
-    correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 0))
-
+    correct = tf.equal(tf.argmax(prediction, 0), tf.argmax(y, 0))
     accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
 
     with tf.Session() as sess:
-
         sess.run(tf.global_variables_initializer())
+        cont = 0
+        r1 = 5
 
-        for p in range(5):
+        start = datetime.datetime.now()
 
-            dictionary = unplicke('data_batch_'+str(p+1))
-            # print(dictionary)
-            print('data_batch_'+str(p+1))
-            array_data = dictionary[b'data']
-            labels = dictionary[b'labels']
+        for j in range(5):
 
-            print("Training...")
-            #  start_time = datetime.datetime.now()
-            for k in range(10000):
+            print("Epoch: ", j)
 
-                batch = array_data[k]  # batch size = 3072
-                batch = mix_channels(batch)
-                # print(batch)
-                # print(len(batch))
-                label = [0] * 10
-                label[labels[k]] = 1
-                # print(label)
+            for p in range(r1):
 
-                sess.run([optimizer, cost], feed_dict={x: batch, y: label, keep_prob: 0.9})
-                # print(sess.run([prediction], feed_dict={x: batch, y: label, keep_prob: 0.9}))
-                progress(k, 10000)
+                dictionary = unplicke('data_batch_'+str(p+1))
+                # print(dictionary)
+                print('\n\tdata_batch_'+str(p+1))
+                array_data = dictionary[b'data']
+                labels = dictionary[b'labels']
 
-                '''if k % 100 == 0:
-                    train_acc = accuracy.eval(feed_dict={x: batch, y: label, keep_prob: 1.0})
-                    print('\nReached step %d with training accuracy %g\n' % (k, train_acc))'''
+                print("\tTraining...")
+                #  start_time = datetime.datetime.now()
+                r = 10000
+                for k in range(r):
 
-            #  time_end = datetime.datetime.now()
-            #  print("\n\nFinished training in", (time_end - start_time))
-            #  print("\nTesting...")
+                    batch = array_data[k]  # batch size = 3072
+                    batch = mix_channels(batch)
+                    # print(batch)
+                    # print(len(batch))
+                    label = [0] * 10
+                    label[labels[k]] = 1
+                    # print(label)
+
+                    sess.run([optimizer, cost], feed_dict={x: batch, y: label, keep_prob: 0.9})
+                    # print(sess.run([prediction], feed_dict={x: batch, y: label, keep_prob: 0.9}))
+                    # progress(cont, r*r1)
+                    cont += 1
+                    '''if k % 100 == 0:
+                        train_acc = accuracy.eval(feed_dict={x: batch, y: label, keep_prob: 1.0})
+                        print('\nReached step %d with training accuracy %g\n' % (k, train_acc))'''
+
+                #  time_end = datetime.datetime.now()
+                #  print("\n\nFinished training in", (time_end - start_time))
+                #  print("\nTesting...")
+
         dictionary = unplicke('test_batch')
         array_test = dictionary[b'data']
         labels_test = dictionary[b'labels']
-        acc = 0
-        for k in range(10000):
+        acc = []
+        r = 10000
+        print("\nTesting...")
+        for k in range(r):
             batch = array_test[k]
             batch = mix_channels(batch)
-            label = [0] * 10
-            label[labels_test[k]] = 1
+            label = [0.0] * 10
+            label[labels_test[k]] = 1.0
             # sess.run([optimizer, cost], feed_dict={x: batch, y: label, keep_prob: 1})
-            acc += accuracy.eval(feed_dict={x: batch, y: label, keep_prob: 1.0})
-            # print(acc, "/", (k+1))
-        print("\nTesting Accuracy: ", acc/10000)
+            # print(correct.eval(feed_dict={x: batch, y: label, keep_prob: 1.0}))
+            # print(accuracy.eval(feed_dict={x: batch, y: label, keep_prob: 1.0}))
+            acc.append(accuracy.eval(feed_dict={x: batch, y: label, keep_prob: 1.0}))
+            # progress(k, r)
+        # print("\n", acc)
+        mean = tf.reduce_mean(acc)
 
+        end = datetime.datetime.now()
+
+        print("\nTesting Accuracy: ", mean.eval())
+
+        print("\nTime: ", (end-start))
 
 def progress(prog, total):  # to show progress bar
     if prog <= 0:
         return
     sys.stdout.write('\r')
-    sys.stdout.write("[%-100s] %d%%" % (u"\u2588"*int(100*((prog+1)/total)), 100*(prog+1)/total))
+    sys.stdout.write("%-100s] %d%%" % (u"\u2588"*int(100*((prog+1)/total)), 100*(prog+1)/total))
     sys.stdout.flush()
 
 
---
+++ oldmain.py
@@ -0,0 +1,93 @@
+import tensorflow as tf
+import os
+import sys
+import datetime
+from tensorflow.examples.tutorials.mnist import input_data
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+mnist = input_data.read_data_sets("/tmp/data", one_hot=True)
+
+n_classes = 10
+batch_size = 50
+x = tf.placeholder('float', [None, 784])
+y = tf.placeholder('float', [None, 10])
+keep_prob = tf.placeholder(tf.float32)  # for dropout
+
+
+def conv2d(x, W):
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+
+def maxpool2d(x):
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+#                               size of window    movement of window
+
+
+def convolutional_neural_network(x):
+
+    weigths = {'w_conv1': tf.Variable(tf.random_normal([5, 5, 1, 32])),
+               'w_conv2': tf.Variable(tf.random_normal([5, 5, 32, 64])),
+               'w_fc': tf.Variable(tf.random_normal([7*7*64, 1024])),
+               'out': tf.Variable(tf.random_normal([1024, n_classes]))}
+
+    biases = {'b_conv1': tf.Variable(tf.random_normal([32])),
+              'b_conv2': tf.Variable(tf.random_normal([64])),
+              'b_fc': tf.Variable(tf.random_normal([1024])),
+              'out': tf.Variable(tf.random_normal([n_classes]))}
+
+    x = tf.reshape(x, shape=[-1, 28, 28, 1])
+
+    conv1 = tf.nn.relu(conv2d(x, weigths['w_conv1'])+biases['b_conv1'])
+    conv1 = maxpool2d(conv1)
+
+    conv2 = tf.nn.relu(conv2d(conv1, weigths['w_conv2'])+biases['b_conv2'])
+    conv2 = maxpool2d(conv2)
+
+    fc = tf.reshape(conv2, [-1, 7*7*64])
+    fc = tf.nn.relu(tf.matmul(fc, weigths['w_fc']) + biases['b_fc'])
+    fc = tf.nn.dropout(fc, keep_prob)
+
+    output = tf.matmul(fc, weigths['out'])+biases['out']
+
+    return output
+
+
+def train_neural_network(x):  # x is input data
+
+    prediction = convolutional_neural_network(x)
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=prediction))
+    optimizer = tf.train.AdamOptimizer().minimize(cost)
+    correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+
+        print("Training...")
+        start_time = datetime.datetime.now()
+
+        for k in range(1000):
+            batch = mnist.train.next_batch(batch_size)
+            sess.run([optimizer, cost], feed_dict={x: batch[0], y: batch[1], keep_prob: 0.8})
+            progress(k, 1000)
+            if k % 100 == 0:
+                train_acc = accuracy.eval(feed_dict={x: batch[0], y: batch[1], keep_prob: 1.0})
+                print('\nReached step %d with training accuracy %g\n' % (k, train_acc))
+
+        time_end = datetime.datetime.now()
+
+        print("\n\nFinished training in", (time_end - start_time))
+        print("\nTesting...")
+        print("Testing Accuracy: ", accuracy.eval(feed_dict={x: mnist.test.images, y: mnist.test.labels, keep_prob: 1.0}))
+
+
+def progress(prog, total):  # to show progress bar
+    if prog <= 0:
+        return
+    sys.stdout.write('\r')
+    sys.stdout.write("%-100s %d%%" % (u"\u2588"*int(100*((prog+1)/total)), 100*(prog+1)/total))
+    sys.stdout.flush()
+
+
+train_neural_network(x)
