2a5a109c0e7a65d7395f6f201d0cedc1d497ad1f
Tiago Leite <tiago.tmleite@gmail.com>
2017-11-09 16:21:11 -0200

    code fixes

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import tensorflow as tf
 import os
 import sys
 import datetime
-import random
 import pickle
 import numpy as np
 
@@ -15,16 +14,13 @@ keep_prob = tf.placeholder(tf.float32)  # for dropout
 datasets = []
 
 
-def mix_channels(array_rgb):
+def resize_array(array_rgb):
     size = int(len(array_rgb)/3)
     array = [float]*size
     max_value = (np.power(2, 24)-1)
-    # print("RGB:\n", array_rgb)
     for k in range(0, int(size)):
         array[k] = array_rgb[k] + array_rgb[k+1024]*256 + array_rgb[k+1024*2]*256*256
         array[k] /= max_value
-    # print(array)
-    # print("max = ", max(array))
     return array
 
 
@@ -57,22 +53,17 @@ def get_batch(start, batch_size, dataset_index):
     labels = dictionary[b'labels']
     batch = [[], []]
     for i in range(start, start+batch_size):
-        batch[0].append(mix_channels(array_data[i]))
-        # print(len(array_data[i]))
+        batch[0].append(resize_array(array_data[i]))
         label_array = np.zeros(n_classes)
         label_array[labels[i]] = 1
         batch[1].append(label_array)
-        # glob_labels[labels[i]] += 1
-        # print(label_array)
-        # print(array_data[i])
-    # print("0:", len(batch[0]))
     return batch
 
 
 def convolutional_neural_network(x):
 
-    weigths = {'w_conv1': tf.Variable(tf.random_normal([5, 5, 1, 32])),
-               'w_conv2': tf.Variable(tf.random_normal([4, 4, 32, 64])),
+    weigths = {'w_conv1': tf.Variable(tf.random_normal([3, 3, 1, 32])),
+               'w_conv2': tf.Variable(tf.random_normal([3, 3, 32, 64])),
                'w_conv3': tf.Variable(tf.random_normal([3, 3, 64, 128])),
                'w_fc': tf.Variable(tf.random_normal([4*4*128, 1024])),
                'w_fc2': tf.Variable(tf.random_normal([1024, 256])),
@@ -108,7 +99,7 @@ def convolutional_neural_network(x):
     return output
 
 
-def train_neural_network(x):  # x is input data
+def train_neural_network(x):  # x is the input data
 
     prediction = convolutional_neural_network(x)
     cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=prediction))
@@ -121,65 +112,25 @@ def train_neural_network(x):  # x is input data
         r1 = 5
         batch_test = get_batch(0, 500, 5)
         start_time = datetime.datetime.now()
-        for j in range(3):
+        print("Training...")
+        for j in range(5):
             print("Epoch: ", j)
             for p in range(r1):
-                # print(dictionary)
-                # file_name = 'data_batch_'+str(p+1)
-                # print(file_name)
-                print("\tTraining...")
-                #  start_time = datetime.datetime.now()
                 r = 200
                 for k in range(r):
                     batch = get_batch(50 * k, 50, p)
-                    # print(len(batch[0][0]))
-                    # print(len(batch[1][0]))
-                    sess.run(optimizer, feed_dict={x: batch[0], y: batch[1], keep_prob: 1.0})
-                    # print(cost.eval(feed_dict={x: batch[0], y: batch[1], keep_prob: 1.0}))
-                    # print(sess.run([prediction], feed_dict={x: batch, y: label, keep_prob: 0.9}))
-                    # progress(cont, r*r1)
-                    # cont += 1
+                    sess.run(optimizer, feed_dict={x: batch[0], y: batch[1], keep_prob: 0.9})
                     if k % 10 == 0:
-                        # train_acc = accuracy.eval(feed_dict={x: batch[0], y: batch[1], keep_prob: 1.0})
-                        # print('Reached step %d in epoch %d with training accuracy %.3f' % (p, j,  train_acc))
-                        # print('\t', len(batch_test[0]))
-                        print("Partial test acc:", k, end=' ')
+                        print("Partial test accuracy:", k, end=' ')
                         print(accuracy.eval(feed_dict={x: batch_test[0], y: batch_test[1], keep_prob: 1.0}))
-
         time_end = datetime.datetime.now()
         print("\n\nFinished training in", (time_end - start_time))
         file_name = 'test_batch'
         print(file_name)
         print("\tTesting...")
-        #  start_time = datetime.datetime.now()
-        batch = get_batch(0, 10000, 5)
-        print(len(batch[0]), ' tests, accuracy =', end=' ')
-        print(accuracy.eval(feed_dict={x: batch[0], y: batch[1], keep_prob: 1.0}))
-
-        '''dictionary = unplicke('test_batch')
-        array_test = dictionary[b'data']
-        labels_test = dictionary[b'labels']
-        acc = []
-        r = 10000
-        print("\nTesting...")
-        for k in range(r):
-            batch = array_test[k]
-            batch = mix_channels(batch)
-            label = [0.0] * 10
-            label[labels_test[k]] = 1.0
-            # sess.run([optimizer, cost], feed_dict={x: batch, y: label, keep_prob: 1})
-            # print(correct.eval(feed_dict={x: batch, y: label, keep_prob: 1.0}))
-            # print(accuracy.eval(feed_dict={x: batch, y: label, keep_prob: 1.0}))
-            acc.append(accuracy.eval(feed_dict={x: batch, y: label, keep_prob: 1.0}))
-            # progress(k, r)
-        # print("\n", acc)
-        mean = tf.reduce_mean(acc)
-
-        end = datetime.datetime.now()
-
-        print("\nTesting Accuracy: ", mean.eval())
-
-        print("\nTime: ", (end-start))'''
+        batch_test = get_batch(0, 10000, 5)
+        print('Testing accuracy =', end=' ')
+        print(accuracy.eval(feed_dict={x: batch_test[0], y: batch_test[1], keep_prob: 1.0}))
 
 
 def progress(prog, total):  # to show progress bar
@@ -192,27 +143,3 @@ def progress(prog, total):  # to show progress bar
 
 load_datasets()
 train_neural_network(x)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
