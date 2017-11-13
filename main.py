311c38e20c810efd48004a52ee379013e6b8a20c
TiagoLeite <tiago.tmleite@gmail.com>
2017-11-13 01:02:15 -0200

    tf tutorial

--- main.py
+++ main.py
@@ -60,8 +60,7 @@ b_fc2 = bias_variable([10])
 y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 
 cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
-
-train_step = tf.train.AdamOptimizer().minimize(cross_entropy)
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
@@ -78,4 +77,12 @@ with tf.Session() as sess:
             print('Step %d, training accuracy %g' % (i, train_acc))
         train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
-    print('Test accuracy %g' % accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+    print("\tTesting...")
+    partials = []
+    for k in range(6):
+        batch = mnist.test.next_batch(1000)
+        res = accuracy.eval(feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
+        partials.append(res)
+        print("Testing Accuracy: ", res)
+    # print(partials)
+    print("Avg:", tf.reduce_mean(partials).eval())
