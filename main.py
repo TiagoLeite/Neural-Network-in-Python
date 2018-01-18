cc5e98d40385cf2161241db1733ccba3028fc051
TiagoLeite <tiago.tmleite@gmail.com>
2018-01-18 17:22:35 -0200

    new model

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from tensorflow.python.tools import freeze_graph
 from tensorflow.python.tools import optimize_for_inference_lib
 import os
 
-# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 EXPORT_DIR = './model'
 if os.path.exists(EXPORT_DIR):
     shutil.rmtree(EXPORT_DIR)
@@ -30,13 +30,11 @@ def read_data(start, end, pattern):
 
 
 def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev=0.1)
-    return tf.Variable(initial)
+    return tf.Variable(tf.truncated_normal(shape, stddev=0.1))
 
 
 def bias_variable(shape):
-    initial = tf.constant(0.1, shape=shape)
-    return tf.Variable(initial)
+    return tf.Variable(tf.ones(shape=shape)/10.0)  # 0.1
 
 
 def conv2d(x, W):
@@ -49,47 +47,62 @@ def max_pool_2x2(x):
 
 print("Reading mnist...")
 # sess = tf.InteractiveSession()
-# mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
 x = tf.placeholder(tf.float32, shape=[None, 784])
-y_ = tf.placeholder(tf.float32, shape=[None, 36])
+y_ = tf.placeholder(tf.float32, shape=[None, 10])
 
 # === Model ===
 
+x_input = tf.reshape(x, [-1, 28, 28, 1])
+
 # Convolutional Layer 1:
-W_conv1 = weight_variable([5, 5, 1, 32])
-b_conv1 = bias_variable([32])
-x_image = tf.reshape(x, [-1, 28, 28, 1])
-h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1)+b_conv1)
-h_pool1 = max_pool_2x2(h_conv1)
+map_size_1 = 6
+w_conv1 = weight_variable([6, 6, 1, map_size_1])
+b_conv1 = bias_variable([map_size_1])
+# h_conv1 = tf.nn.relu(conv2d(x_input, w_conv1) + b_conv1)
+# h_pool1 = max_pool_2x2(h_conv1)
+y_conv1 = tf.nn.relu(tf.nn.conv2d(x_input, w_conv1, strides=[1, 1, 1, 1], padding='SAME') + b_conv1)
 
 # Convolutional Layer 2:
-W_conv2 = weight_variable([5, 5, 32, 64])
-b_conv2 = bias_variable([64])
-h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2)+b_conv2)
-h_pool2 = max_pool_2x2(h_conv2)
+map_size_2 = 12
+w_conv2 = weight_variable([5, 5, map_size_1, map_size_2])
+b_conv2 = bias_variable([map_size_2])
+# h_conv2 = tf.nn.relu(conv2d(h_pool1, w_conv2) + b_conv2)
+# h_pool2 = max_pool_2x2(h_conv2)
+y_conv2 = tf.nn.relu(tf.nn.conv2d(y_conv1, w_conv2, strides=[1, 2, 2, 1], padding='SAME') + b_conv2)
+y_conv2_pool = tf.nn.max_pool(y_conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+# Convolutional Layer 3:
+map_size_3 = 24
+w_conv3 = weight_variable([4, 4, map_size_2, map_size_3])
+b_conv3 = bias_variable([map_size_3])
+# h_conv2 = tf.nn.relu(conv2d(h_pool1, w_conv2) + b_conv2)
+# h_pool2 = max_pool_2x2(h_conv2)
+y_conv3 = tf.nn.relu(tf.nn.conv2d(y_conv2, w_conv3, strides=[1, 2, 2, 1], padding='SAME') + b_conv3)
+y_conv3_pool = tf.nn.max_pool(y_conv3, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
 # Fully connected layer:
-W_fc1 = weight_variable([7*7*64, 1024])
-b_fc1 = bias_variable([1024])
-h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
-h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1)+b_fc1)
+fc_input = tf.reshape(y_conv3, [-1, 7 * 7 * map_size_3])
+w_fc1 = weight_variable([7 * 7 * map_size_3, 256])
+b_fc1 = bias_variable([256])
+y_fc1 = tf.nn.relu(tf.matmul(fc_input, w_fc1) + b_fc1)
 
 # Dropout:
 keep_prob = tf.placeholder(tf.float32)
-h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+y_fc1_drop = tf.nn.dropout(y_fc1, keep_prob)
 
 # Read out layer:
-W_fc2 = weight_variable([1024, 36])
-b_fc2 = bias_variable([36])
+w_fc2 = weight_variable([256, 10])
+b_fc2 = bias_variable([10])
 
-y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+y_out = tf.nn.softmax(tf.matmul(y_fc1_drop, w_fc2) + b_fc2)
 
 # =========
 
-loss_cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
+loss_cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_out))
 train_step = tf.train.AdamOptimizer(1e-4).minimize(loss_cross_entropy)
-correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+correct_prediction = tf.equal(tf.argmax(y_out, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 print("Training...")
@@ -101,27 +114,42 @@ with tf.Session() as sess:
     start = datetime.datetime.now()
 
     for p in range(1):
-        for i in range(180):
-            # batch = mnist.train.next_batch(100)
-            batch_font = read_data(i*5, (i+1)*5, 'fnt/Sample%.3d/img%.3d-%.5d.png')
+
+        for i in range(2000):
+            batch = mnist.train.next_batch(100)
             if i % 50 == 0:
+                train_acc = accuracy.eval(feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
+                print('Step %3d, digit training accuracy %g' % (i, train_acc))
+            train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.75})
+            # batch_font = read_data(i*5, (i+1)*5, 'fnt/Sample%.3d/img%.3d-%.5d.png')
+            '''if i % 50 == 0:
                 train_acc = accuracy.eval(feed_dict={x: batch_font[0], y_: batch_font[1], keep_prob: 1.0})
                 print('Step %3d/180/%d, font digit training accuracy %g' % (i, p, train_acc))
             train_step.run(feed_dict={x: batch_font[0], y_: batch_font[1], keep_prob: 0.5})
-        for k in range(5):
+            for k in range(5):
             for i in range(12):
                 # batch = mnist.train.next_batch(100)
                 batch_hand = read_data(i*4, (i+1)*4, 'handwritten/Sample%.3d/img%.3d-%.3d.png')
                 if k == 0 and i == 0:
                     train_acc = accuracy.eval(feed_dict={x: batch_hand[0], y_: batch_hand[1], keep_prob: 1.0})
                     print('Step %3d, hand digit training accuracy %g' % (p, train_acc))
-                train_step.run(feed_dict={x: batch_hand[0], y_: batch_hand[1], keep_prob: 0.5})
+                train_step.run(feed_dict={x: batch_hand[0], y_: batch_hand[1], keep_prob: 0.5})'''
 
     end = datetime.datetime.now()
     print("\nFinished training in", (end - start))
 
     print("\tTesting...")
+    mean = []
 
+    for _ in range(10):
+        batch = mnist.test.next_batch(100)
+        acc = accuracy.eval(feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
+        print(acc)
+        mean.append(acc)
+
+    print('Media: ', tf.reduce_mean(mean).eval())
+
+    '''
     batch_hand = read_data(48, 55, 'handwritten/Sample%.3d/img%.3d-%.3d.png')
     res = accuracy.eval(feed_dict={x: batch_hand[0], y_: batch_hand[1], keep_prob: 1.0})
     print("Testing Hand Digit Accuracy: ", res)
@@ -137,18 +165,18 @@ with tf.Session() as sess:
     print("Testing Font Digit Accuracy: ", res)
     batch_font = read_data(975, 1000, 'fnt/Sample%.3d/img%.3d-%.5d.png')
     res = accuracy.eval(feed_dict={x: batch_font[0], y_: batch_font[1], keep_prob: 1.0})
-    print("Testing Font Digit Accuracy: ", res)
+    print("Testing Font Digit Accuracy: ", res)'''
 
-    W_C1 = W_conv1.eval(sess)
+    W_C1 = w_conv1.eval(sess)
     B_C1 = b_conv1.eval(sess)
 
-    W_C2 = W_conv2.eval(sess)
+    W_C2 = w_conv2.eval(sess)
     B_C2 = b_conv2.eval(sess)
 
-    W_FC1 = W_fc1.eval(sess)
+    W_FC1 = w_fc1.eval(sess)
     B_FC1 = b_fc1.eval(sess)
 
-    W_FC2 = W_fc2.eval(sess)
+    W_FC2 = w_fc2.eval(sess)
     B_FC2 = b_fc2.eval(sess)
 
 graph = tf.Graph()
